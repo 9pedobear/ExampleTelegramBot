bd5ac0823251039f63ae638e7573fc5e1a9abb37
kaira96 <kayratsagynbekov@gmail.com>
2022-04-01 15:54:14 +0600

    18 кнопка работает

--- main.py
+++ main.py
@@ -32,6 +32,10 @@ async def start_command(message: types.Message):
     inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
     await message.answer("Rick and Morty", reply_markup=inline_kb1)
 
+@dp.callback_query_handler()
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
 
 
 # for name in characters_raw:
