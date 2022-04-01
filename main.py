e3c9e7f31e629dc45ccd8f680dcea604ce7d2779
kaira96 <kayratsagynbekov@gmail.com>
2022-04-01 15:59:24 +0600

    19 все работает

--- main.py
+++ main.py
@@ -25,18 +25,49 @@ characters = {}
 characters_names = []
 
 
+inline_btn_1 = InlineKeyboardButton('Первая кнопка!',
+                                        callback_data='button1')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
+inline_kb_full = InlineKeyboardMarkup(row_width=2).add(inline_btn_1)
+inline_kb_full.add(
+    InlineKeyboardButton('Вторая кнопка', callback_data='btn2'))
+inline_btn_3 = InlineKeyboardButton('кнопка 3', callback_data='btn3')
+inline_btn_4 = InlineKeyboardButton('кнопка 4', callback_data='btn4')
+inline_btn_5 = InlineKeyboardButton('кнопка 5', callback_data='btn5')
+inline_kb_full.add(inline_btn_3, inline_btn_4, inline_btn_5)
+inline_kb_full.row(inline_btn_3, inline_btn_4, inline_btn_5)
+inline_kb_full.insert(
+    InlineKeyboardButton("query=''", switch_inline_query=''))
+inline_kb_full.insert(
+    InlineKeyboardButton("query='qwerty'", switch_inline_query='qwerty'))
+inline_kb_full.insert(InlineKeyboardButton("Inline в этом же чате",                                              switch_inline_query_current_chat='wasd'))
+inline_kb_full.add(InlineKeyboardButton('Уроки aiogram',                     url='https://surik00.gitbooks.io/aiogram-lessons/content/'))
+
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    inline_btn_1 = InlineKeyboardButton('Первая кнопка!',
-                                        callback_data='button1')
-    inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
     await message.answer("Rick and Morty", reply_markup=inline_kb1)
 
 @dp.callback_query_handler()
 async def process_callback_button1(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
+    code = callback_query.data[-1]
+    if code.isdigit():
+        code = int(code)
+    if code == 2:
+        await bot.answer_callback_query(callback_query.id,
+                                        text='Нажата вторая кнопка')
+    elif code == 5:
+        await bot.answer_callback_query(
+            callback_query.id,
+            text='Нажата кнопка с номером 5.\nА этот текст может быть длиной до 200 символов 😉',
+            show_alert=True)
+    else:
+        await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id,
+                           f'Нажата инлайн кнопка! code={code}')
 
+@dp.message_handler(commands=['2'])
+async def process_command_2(message: types.Message):
+    await message.reply("Отправляю все возможные кнопки", reply_markup=inline_kb_full)
 
 # for name in characters_raw:
 #     characters_names.append((name['name']))
