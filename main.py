81f3f48daecb397a910edd7723c77ed2cb4756e2
kaira96 <kayratsagynbekov@gmail.com>
2022-04-01 13:47:52 +0600

    2

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
-
+from pprint import pprint as pp
 
 page = input("Введите номер страницы: ")
 main_request = requests.get(f'https://rickandmortyapi.com/api/character?page'
                             f'={page}')
 
-print(main_request.json())
+all_data = main_request.json()
+characters_raw = all_data['results']
+pp(characters_raw)
+
