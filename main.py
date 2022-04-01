8fe71f52f160198733d0646773c6d9d16d5b1bfc
kaira96 <kayratsagynbekov@gmail.com>
2022-04-01 15:00:10 +0600

    6

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import requests
 from pprint import pprint as pp
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+tg_bot_token = '5163185542:AAG96jJfS85lftHx4F1BBozDTJBLncC1h28'
+
+bot = Bot(token=tg_bot_token)
+dp = Dispatcher(bot)
 
 page = input("Введите номер страницы: ")
 main_request = requests.get(f'https://rickandmortyapi.com/api/character?page'
@@ -12,12 +20,17 @@ prev_page_url = all_data['info']['prev']
 characters = {}
 characters_names = []
 
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer("Rick and Morty")
+
+
 for name in characters_raw:
     characters_names.append((name['name']))
 
 for id in characters_raw:
     characters[id['id']] = id
 
-pp(characters)
 
-
+executor.start_polling(dp)
